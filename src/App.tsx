dc6e23c082b7d45b3bd0ee229f95e5fd32222655
origuri <dlcksrb7@naver.com>
2023-12-26 14:39:25 +0900

    2023.12.26 - drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Variants, motion } from "framer-motion";
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -10,59 +10,60 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: #3498db;
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden; // bigBox 영역에서 넘치면 나간만큼 숨겨줌
+`;
+
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgb(255, 255, 255);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVars = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
+  // hover하면 크기가 1.5배 커짐, 180도 회전
+  hover: {
+    scale: 1.5,
+    rotateZ: 180,
+  },
+  // 해당 div를 계속 클릭을 유지할 때 크기는 원상 복귀, 원모양 변환
+  tab: {
     scale: 1,
+    borderRadius: "100px",
+  },
+  drag: {
+    backgroundColor: "#9b59b6",
     transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      // duration이 0.5이므로 부모가 나타나는게 완료 되고 0.3초 이따 시작
-      delayChildren: 0.3,
-      staggerChildren: 0.2, // 자식 컴포넌트들의 css가 0.5초 간격으로 실행
+      duration: 0, // 애니메이션 효과를 주기 싫을 때
     },
   },
 };
 
-const circleVars: Variants = {
-  // y는 +면 아래 -면 위부터 시작
-  start: { opacity: 0, y: 30 },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
-};
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        {/* 프로퍼티가 부모와 같아서 생략해도 자동으로 적용된다. */}
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          variants={boxVars}
+          whileHover={"hover"}
+          whileTap={"tab"}
+          whileDrag={"drag"}
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
